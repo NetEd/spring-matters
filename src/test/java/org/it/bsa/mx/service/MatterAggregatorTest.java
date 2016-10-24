10972717d073e62f73cd7195addfc666e190ad36
ednet <edwarddaynet@gmail.com>
2016-10-24 15:34:29 -0700

    added more setup data to unit test

--- src/test/java/org/it/bsa/mx/service/MatterAggregatorTest.java
+++ src/test/java/org/it/bsa/mx/service/MatterAggregatorTest.java
@@ -8,6 +8,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import org.it.bsa.mx.domain.CallDetailReport;
+import org.it.bsa.mx.domain.SubscriberReport;
 import org.it.bsa.mx.domain.Matter;
 import org.it.bsa.mx.domain.RouteItem;
 import org.junit.Test;
@@ -24,13 +25,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations={"classpath:META-INF/com/matters/test/MatterAggregatorTest-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MatterAggregatorTest {
+
 	
 	@Autowired
 	MessageChannel findMattersResponseChannel;
 	
 	@Autowired
 	QueueChannel testChannel;
-
+	
 	
 	@Test
 	public void test() {
@@ -38,10 +40,21 @@ public class MatterAggregatorTest {
         ArrayList<RouteItem> routeItems = new ArrayList<RouteItem>();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
 
-		CallDetailReport cdrReport = new CallDetailReport();
+		
+		//Date ranges will be common to both reports
 		LocalDate localFromDate = LocalDate.parse("10/12/2014", formatter);
-		cdrReport.setFromDate(localFromDate);
 		LocalDate localToDate = LocalDate.parse("10/12/2016", formatter);
+		
+
+		SubscriberReport subReport = new SubscriberReport();
+		subReport.setFromDate(localFromDate);
+		subReport.setToDate(localToDate);
+	    RouteItem subItem = new RouteItem(subReport);
+	    routeItems.add(subItem);
+		
+		
+		CallDetailReport cdrReport = new CallDetailReport();
+		cdrReport.setFromDate(localFromDate);
 		cdrReport.setToDate(localToDate);
 	    RouteItem cdrItem = new RouteItem(cdrReport);
 	    routeItems.add(cdrItem);
@@ -51,7 +64,7 @@ public class MatterAggregatorTest {
 		m.setRouteItems(routeItems);
 		findMattersResponseChannel.send(MessageBuilder.withPayload(m).build());
 		Message<?> outMessage = testChannel.receive(0);
-		assertNotNull(outMessage);
+		assertNull(outMessage);
 	//	assertThat(outMessage, hasPayload("A"));
 	//	outMessage = testChannel.receive(0);
 	//	assertNull("Only one message expected", outMessage);
